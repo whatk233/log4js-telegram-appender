ae533c2cfef126283e3863a1471d90181f6c3b10
Fyodor <haverev.feodor@gmail.com>
2021-05-20 18:31:08 +0300

    moved from request to got. updated peer dep

--- index.js
+++ index.js
@@ -1,81 +1,60 @@
 'use strict';
 
-const request = require("request");
+const got = require("got");
 
 // This is the function that generates an appender function
 function stdoutAppender(layout, levels, config) {
 
-    var silentLevel = levels.getLevel(config.silentAlertLevel);
-    var audioLevel = levels.getLevel(config.audioAlertLevel);
-
-    var botchat_params = {
-        method: "GET",
-        uri: "https://api.telegram.org/bot"+config.bottoken+"/sendMessage",
-        qs: {
+    const silentLevel = levels.getLevel(config.silentAlertLevel);
+    const audioLevel = levels.getLevel(config.audioAlertLevel);
+    const sentMessageRoute = `https://api.telegram.org/bot/${config.bottoken}/sendMessage`;
+    const botchat_params = {
+        searchParams: {
             chat_id: config.botchatid,
             parse_mode: "html"
         }
     };
     // This is the appender function itself
-    const appender = (loggingEvent) => {
-        var params = JSON.parse(JSON.stringify(botchat_params));
+    return async (loggingEvent) => {
+        const params = JSON.parse(JSON.stringify(botchat_params));
         if (silentLevel.isLessThanOrEqualTo(loggingEvent.level.levelStr)) {
             //console.log(`===== silentAlertLevel is less than loggingEvent level.`);
             if (audioLevel.isLessThanOrEqualTo(loggingEvent.level.levelStr)) {
                 //console.log(`===== log telegram with sound`);
-                Object.assign(params.qs, {
+                Object.assign(params.searchParams, {
                     text: layout(loggingEvent),
                     disable_notification: false
                 });
-                request(params, (error, response, body) => {
-                    if (error) {
-                        console.log("Error sending to telegram:");
-                        console.log(error);
-                    } /* else {
-                        console.log("Message sent to telegram:");
-                        console.log(body);
-                    }*/
-                });
+                try {
+                    await got(sentMessageRoute, params);
+                } catch (error) {
+                    console.error("Error sending to telegram:");
+                    console.error(error);
+                }
             } else {
                 //console.log(`===== log telegram quietly`);
-                Object.assign(params.qs, {
+                Object.assign(params.searchParams, {
                     text: layout(loggingEvent),
                     disable_notification: true
                 });
-                request(params, (error, response, body) => {
-                    if (error) {
-                        console.log("Error sending to telegram:");
-                        console.log(error);
-                    } /* else {
-                        console.log("Message sent to telegram:");
-                        console.log(body);
-                    } */
-                });
+                try {
+                    await got(sentMessageRoute, params);
+                } catch (error) {
+                    console.error("Error sending to telegram:");
+                    console.error(error);
+                }
             }
-        } /* else {
-            console.log(`===== silentAlertLevel is greater than or equal to loggingEvent level.\n===== don't log telegram`);
-        } */
-
-        //process.stdout.write(`test: ${layout(loggingEvent)}\n`);
+        }
     };
-
-    // add a shutdown function.
-    /*
-    appender.shutdown = (done) => {
-        process.stdout.write('appender shutdown', done);
-    };
-    */
-
-    return appender;
 }
 
 function configure(config, layouts, findAppender, levels) {
 
     // the default custom layout for this appender, not using the layouts module
-    var default_layout = function(loggingEvent) {
-        var header = `<b>${loggingEvent.categoryName}: ${loggingEvent.level}</b>\n`;
-        var timestamp = `[${loggingEvent.startTime.toISOString()}]\n`;
-        var body = loggingEvent.data.map(d => {
+    const default_layout = function(loggingEvent) {
+        const header = `<b>${loggingEvent.categoryName}: ${loggingEvent.level}</b>\n`;
+        const timestamp = `[${loggingEvent.startTime.toISOString()}]\n`;
+        const body = loggingEvent.data.map(d => {
             try {
                 if (typeof d === 'object') return JSON.stringify(d);
             } catch (e) {
@@ -87,7 +66,7 @@ function configure(config, layouts, findAppender, levels) {
         return header+timestamp+body;
     };
 
-    var use_layout = config.layout ? layouts.layout(config.layout.type, config.layout) : default_layout;
+    const use_layout = config.layout ? layouts.layout(config.layout.type, config.layout) : default_layout;
 
     //create a new appender instance
     return stdoutAppender(use_layout, levels, config);
@@ -95,6 +74,3 @@ function configure(config, layouts, findAppender, levels) {
 
 //export the only function needed
 exports.configure = configure;
-
-
-
